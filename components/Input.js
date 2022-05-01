3d2b7c3c013e502c79d0c528f0b8ff87583528e3
MassyWeeWee <wyattconnollyfitness@gmail.com>
2022-04-30 17:06:42 -0700

    styling for feed, input, sidebar and sidebarlink

---
+++ components/Input.js
@@ -0,0 +1,81 @@
+import {
+  CalendarIcon,
+  ChartBarIcon,
+  EmojiHappyIcon,
+  PhotographIcon,
+  XIcon,
+} from "@heroicons/react/outline";
+import { useState, useRef } from "react";
+
+function Input() {
+  const [input, setInput] = useState();
+  const [selectedFile, setSelectedFile] = useState();
+  const filePickerRef = useRef(null);
+
+  const addImageToPost = () => {};
+
+  return (
+    <div
+      className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll scrollbar-hide`}
+    >
+      <img src="" alt="" className="h-11 w-11 rounded-full cursor-pointer" />
+      <div className="divide-y divide-gray-700 w-full">
+        <div className={`${selectedFile && "pb-7"} ${input && "space-y-2.5"}`}>
+          <textarea
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="What's happening?"
+            rows="2"
+            className="bg-transparent outline-none text-[#d9d9d9] text-lg placeholder-gray-500 tracking-wide w-full min-h-[50px]"
+          />
+
+          {selectedFile && (
+            <div className="relative">
+              <div
+                className="absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75 rounded-full flex items-center justify-center top-1 left-1 cursor-pointer"
+                onClick={() => setSelectedFile(null)}
+              >
+                <XIcon className="text-white h-5" />
+              </div>
+              <img
+                src={selectedFile}
+                alt=""
+                className="rounded-2xl max-h-80 object-contain"
+              />
+            </div>
+          )}
+        </div>
+        <div className="flex items-center justify-between pt-2.5">
+          <div className="flex items-center">
+            <div className="icon" onClick={() => filePickerRef.current.click()}>
+              <PhotographIcon className="text-[#1d9bf0] h-[22px]" />
+              <input
+                type="file"
+                ref={filePickerRef}
+                hidden
+                onChange={addImageToPost}
+              />
+            </div>
+            <div className="icon rotate-90">
+              <ChartBarIcon className="text-[#1d9bf0] h-[22px]" />
+            </div>
+            <div className="icon" onClick={() => setShowEmojis(!showEmojis)}>
+              <EmojiHappyIcon className="text-[#1d9bf0] h-[22px]" />
+            </div>
+            <div className="icon">
+              <CalendarIcon className="text-[#1d9bf0] h-[22px]" />
+            </div>
+          </div>
+          <button
+            className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-default"
+            disabled={!input && !selectedFile}
+          >
+            Tweet
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Input;
