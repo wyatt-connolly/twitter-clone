5c7e64e0509c6c75cd473cf5520318d845b2d081
MassyWeeWee <wyattconnollyfitness@gmail.com>
2022-05-05 17:12:56 -0700

    recoil state management & post component

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,26 @@
 import { SparklesIcon } from "@heroicons/react/outline";
+import { useEffect, useState } from "react";
 import Input from "./Input";
+import { onSnapshot, collection, query, orderBy } from "@firebase/firestore";
+import { db } from "../firebase";
+import Post from "./Post";
+import { useSession } from "next-auth/react";
 
 function Feed() {
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+    [db]
+  );
+
   return (
     <div className="flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
       <div className="text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-black border-b border-gray-700">
@@ -10,7 +29,13 @@ function Feed() {
           <SparklesIcon className="h-5 text-white" />
         </div>
       </div>
+
       <Input />
+      <div className="pb-72">
+        {posts.map((post) => (
+          <Post key={post.id} id={post.id} post={post.data()} />
+        ))}
+      </div>
     </div>
   );
 }
